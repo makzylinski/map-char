02b3f5d84ac1f5bc39887c8ab075e790f49e1f58
Maks <makzylinski@gmail.com>
2024-10-29 22:06:41 +0100

    state manipulation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,39 @@
+import { useState } from "react";
+
 export default function Form() {
-  function handleForm(e: any) {
-    console.log(e.target.value);
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredColor, setEnteredColor] = useState("");
+
+  function handleNameChange(event: any) {
+    setEnteredName(event?.target.value);
   }
+
+  function handleColorChange(event: any) {
+    setEnteredColor(event?.target.value);
+  }
+
   return (
     <form className="form">
       <h2>Player Form</h2>
 
       <div className="form__row">
         <label htmlFor="name">Name</label>
-        <input id="name" type="text" onChange={handleForm} />
+        <input
+          id="name"
+          type="text"
+          onChange={handleNameChange}
+          value={enteredName}
+        />
       </div>
 
       <div className="form__row">
         <label htmlFor="color">Color</label>
-        <select name="color" id="color">
+        <select
+          name="color"
+          id="color"
+          onChange={handleColorChange}
+          value={enteredColor}
+        >
           <option value="red">Red</option>
           <option value="green">Green</option>
           <option value="blue">Blue</option>
