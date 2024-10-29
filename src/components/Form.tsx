d1dc90b711a7df4ee47348ccd5b01900be97ea8d
Maks <makzylinski@gmail.com>
2024-10-29 21:53:25 +0100

    added simple form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,24 @@
 export default function Form() {
+  function handleForm(e: any) {
+    console.log(e.target.value);
+  }
   return (
-    <>
-      <p>Form component works!</p>
-    </>
+    <form className="form">
+      <h2>Player Form</h2>
+
+      <div className="form__row">
+        <label htmlFor="name">Name</label>
+        <input id="name" type="text" onChange={handleForm} />
+      </div>
+
+      <div className="form__row">
+        <label htmlFor="color">Color</label>
+        <select name="color" id="color">
+          <option value="red">Red</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+        </select>
+      </div>
+    </form>
   );
 }
